93c55ea91953ec088457892b0871e2157825907a
Sleepy_chord <sj702d5@foxmail.com>
2017-05-28 17:29:56 +0800

    design

--- src/tomasulo/UserWindow.java
+++ src/tomasulo/UserWindow.java
@@ -1,6 +1,7 @@
 package tomasulo;
 
 import java.awt.FlowLayout;
+import java.util.ArrayList;
 
 import javax.swing.*; 
 
@@ -10,14 +11,44 @@ public class UserWindow {
 	JPanel panetop,panecenter;
 	JTable Instr,state,loadq,stroeq,station,funit,runit,mem;
 	JLabel clock;
+	//resources
+	LogicInterface logic;
+	ArrayList<Command> cs;
+	double[] memory;
+	int[] registers;
+	int startAddr;//displayed start addr
+	UniversalActionListener listener;
 	
-	public void writeTable(){
-		
+	public void runOneCycle(){
+		//Logic.runCycle & updateTable
 	}
-	public void reTable(){
-		
+	public void loadCommands(){
+		//Helper.readCommandsFromFileByDialog & updateTable
+	}
+	public void reset(){
+		//Logic.clear() & updateTable, clock, pc...
+	}
+	public void openEditableMode(){
+		//disable some buttons
+	}
+	public void closeEditableMode(){
+		//need to writeTable() & restore buttons
+	}
+	public void writeTable(){
+		//set values in Registers, memory and commands tables to logic
+	}
+	public void updateTable(){
+		//get all data from logic & set them in table, update
+	}
+	public void displayMemory(int addr){
+		//update table
 	}
 	public UserWindow(){
+		logic = new Logic();
+		memory = logic.getMemory();
+		registers = logic.getRegisters();
+		cs = logic.getCommands();
+		
 		mainframe = new JFrame();
 		mainframe.setTitle("Tomasulo");
 		mainframe.setSize(600, 400);
@@ -47,7 +78,6 @@ public class UserWindow {
 	
 	public static void main(String args[]){
 		new UserWindow();
-		
 	}
     
     
