bffdb2256d5be02b7e0513d590a7f6f0d1c46d30
王红梅 <hm-wang14@mails.tsinghua.edu.cn>
2017-05-28 22:43:24 +0800

    Table Init

--- src/tomasulo/UserWindow.java
+++ src/tomasulo/UserWindow.java
@@ -68,6 +68,7 @@ public class UserWindow {
 			 }				 
 		}
 		//-------TODO others
+		for(int i=0;i<)
 	}
 	public void displayMemory(int addr){
 		//update table
@@ -80,7 +81,7 @@ public class UserWindow {
 		
 		mainframe = new JFrame();
 		mainframe.setTitle("Tomasulo");
-		mainframe.setSize(600, 400);
+		mainframe.setSize(1000, 800);
 		
 		listener = new UniversalActionListener(this);
 		
@@ -134,6 +135,51 @@ public class UserWindow {
 		instr.getTableHeader().setVisible(true);
 		panecenter.add(new JScrollPane(instr));//or the header will not display!
 		//-------TODO others
+		//------load/store table
+		final Object[] loadAndStoreColumnNames = 
+			{"Name", "Busy", "Addr"};
+		Object[][] loadData = {{"Load1","No",""},{"Load2","No",""},{"Load3","No",""}};
+		loadq = new JTable(loadData,loadAndStoreColumnNames);
+		loadq.getTableHeader().setVisible(true);
+		panecenter.add(loadq);
+		
+		Object[][] storeData = {{"Store1","No",""},{"Store2","No",""},{"Store3","No",""}};
+		storeq = new JTable(storeData,loadAndStoreColumnNames);
+		storeq.getTableHeader().setVisible(true);
+		panecenter.add(storeq);
+		//------stations table
+		final Object[] stationColumnNames = 
+			{"Time", "Name", "Busy", "Op", "Vj", "Vk", "Qj", "Qk"};
+		Object[][] statData = {
+				{"","Add1","No","","","","",""},
+				{"","Add2","No","","","","",""},
+				{"","Add3","No","","","","",""},
+				{"","Mult1","No","","","","",""},
+				{"","Mult2","No","","","","",""}};
+		station = new JTable(statData,stationColumnNames);
+		station.getTableHeader().setVisible(true);
+		panecenter.add(station);
+		//------funit/runit table
+		final Object[] fuColumnNames = {"F0","F1","F2","F3","F4","F5","F6","F7","F8",
+				"F9","F10","F11","F12","F13","F14","F15","F16","F17","F18","F19","F20",
+				"F21","F22","F23","F24","F25","F26","F27","F28","F29","F30"};
+		Object[][] fuData = new Object[][]{};
+		funit = new JTable(fuData,fuColumnNames);
+		funit.getTableHeader().setVisible(true);
+		panecenter.add(funit);
+		
+		final Object[] ruColumnNames = {"R0","R1","R2","R3","R4","R5","R6","R7","R8",
+				"R9","R10","R11","R12","R13","R14","R15","R16","R17","R18","R19","R20",
+				"R21","R22","R23","R24","R25","R26","R27","R28","R29","R30"};
+		Object[][] ruData = new Object[][]{};
+		runit = new JTable(ruData,ruColumnNames);
+		runit.getTableHeader().setVisible(true);
+		panecenter.add(runit);
+		//------mem table
+		mem = new JTable(2,6);
+		mem.setVisible(true);
+		panecenter.add(mem);
+		
 		updateTable();
 		panecenter.setVisible(true);
 		mainframe.setVisible(true);
